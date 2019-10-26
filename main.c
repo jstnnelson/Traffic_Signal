74ecf2da39adb03594ee2066524545e9b7f2d216
jstnnelson <jstnnelson@gmail.com>
2019-10-25 22:53:45 -0500

    Basic signal progression implemented

--- main.c
+++ main.c
@@ -8,22 +8,41 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define MIN_GREEN 5000
+#define MAX_GREEN 5000
+#define YELLOW_CLEARANCE 3000 
+#define RED_CLEARANCE 1000
+
 void init();
 
 int main(void)
 {
-    /* Replace with your application code */
     while (1) 
     {
-		PORTB &= ~(1<<PORTB1);
-		PORTB |= 1<<PORTB3;
-		_delay_ms(1000);
-		PORTB &= ~(1<<PORTB3);
-		PORTB |= 1<<PORTB2;
-		_delay_ms(1000);
-		PORTB &= ~(1<<PORTB2);
+		//Green signal one direction 
+		PORTB &= ~(1<<PORTB3);		
 		PORTB |= 1<<PORTB1;
-		_delay_ms(1000);		
+		_delay_ms(MIN_GREEN);
+		//Change green to yellow
+		PORTB &= ~(1<<PORTB1);		
+		PORTB |= 1<<PORTB2;		
+		_delay_ms(YELLOW_CLEARANCE);
+		//Change yellow to red
+		PORTB &= ~(1<<PORTB2);		
+		PORTB |= 1<<PORTB3;		
+		_delay_ms(RED_CLEARANCE);
+		//Green signal other direction
+		PORTD &= ~(1<<PORTD6);
+		PORTB |= 1<<PORTB0;	
+		_delay_ms(MIN_GREEN);
+		//Change green to yellow	
+		PORTB &= ~(1<<PORTB0);
+		PORTD |= 1<<PORTD7;		
+		_delay_ms(YELLOW_CLEARANCE);
+		//Change yellow to red
+		PORTD &= ~(1<<PORTD7);
+		PORTD |= 1<<PORTD6;		
+		_delay_ms(RED_CLEARANCE);	
     }
 }
 
@@ -31,9 +50,8 @@ void init()
 {
 	DDRB |= ((1<<DDB0) | (1<<DDB1) | (1<<DDB2) | (1<<DDB3));
 	DDRD |= ((1<<DDD6) | (1<<DDD7));
-	PORTB |= ((1<<PORTB0) | (1<<PORTB1) | (1<<PORTB2) | (1<<PORTB3));
-	PORTD |= ((1<<PORTD6) | (1<<PORTD7));
-	//PINB &= (~(1<<PINB0) & ~(1<<PINB1) & ~(1<<PINB2) & ~(1<<PINB3));
-	//PIND &= (~(1<<PIND6) & ~(1<<PIND7)); 
+	PORTB |= 1<<PORTB3;
+	PORTD |= 1<<PORTD6;	
+	_delay_ms(2000);	
 }
 
